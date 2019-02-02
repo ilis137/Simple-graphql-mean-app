b3993b0c97b7c764a70cdd044a212f1900502de3
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-02 16:36:15 +0530

    connect to mongodb

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const {ApolloServer}=require("apollo-server-express")
-
+const mongoose=require("mongoose")
 const app=require("express")()
+
 const typeDefs=require("./schema/typedefs.js")
 const resolvers=require("./schema/resolvers.js")
 
+require("dotenv").config()
 
-
-
-
-
+mongoose.promise=global.promise
+mongoose.connect(process.env.mongoURL,{useNewUrlParser:true},()=>{
+  console.log("successfully connected to mongodb")
+})
 const server=new ApolloServer({typeDefs,resolvers})
 
 server.applyMiddleware({app})
